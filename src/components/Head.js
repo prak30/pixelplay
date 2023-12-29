158f9caef4ce1472708f4834f4f3654ae1ed984f
pranav <katkarpranav4@gmail.com>
2023-12-30 02:41:47 +0530

    built header eith grid

--- src/components/Head.js
+++ src/components/Head.js
@@ -3,26 +3,24 @@ import { HAMBURGER_LOGO, YOUTUBE_LOGO } from "../utils/common";
 
 const Head = () => {
   return (
-    <div className="flex bg-slate-200 justify-between w-auto h-12">
-      <div className="flex mx-2">
-        <img className="mr-2" src={HAMBURGER_LOGO} alt="hamburger" />
-        <img className="ml-2" src={YOUTUBE_LOGO} alt="youtube" />
+    <div className="grid grid-flow-col p-5 m-2 shadow-lg">
+      <div className="flex col-span-1">
+        <img className="h-8" src={HAMBURGER_LOGO} alt="hamburger" />
+        <img className="h-8" src={YOUTUBE_LOGO} alt="youtube" />
       </div>
-      <div className="flex">
+      <div className="col-span-10">
         <input
-          className="w-96 px-4 py-2 border border-gray-400  rounded-l-full"
+          className="w-1/2 border border-gray-400 p-1  rounded-l-full"
           type="text"
           placeholder="search..."
         />
-        <input
-          className="w-8 py-2 border border-gray-400   rounded-r-full"
-          type="text"
-          placeholder="🔍"
-        />
+        <button className="border border-gray-400 p-1 rounded-r-full">
+          🔍
+        </button>
       </div>
-      <div>
+      <div className="col-span-1">
         <img
-          className="h-12"
+          className="h-8"
           src="https://yt3.ggpht.com/yti/AGOGRCrHOQEbZWlBVMy1WBL3e_WxWMsfS_6NhGxwWFxTyZ0=s88-c-k-c0x00ffffff-no-rj"
           alt="profile"
         />
