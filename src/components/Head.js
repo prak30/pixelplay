090f38e5ae494520fd524b8d847e5908cc84f757
pranav <katkarpranav4@gmail.com>
2024-01-03 01:37:18 +0530

    added search with debouncing

--- src/components/Head.js
+++ src/components/Head.js
@@ -2,8 +2,25 @@ import React from "react";
 import { HAMBURGER_LOGO, YOUTUBE_LOGO } from "../utils/common";
 import { useDispatch } from "react-redux";
 import { toggleMenu } from "../utils/appSlice";
+import { YOUTUBE_SEARCH_API } from "../utils/common";
+import { useState, useEffect } from "react";
 
 const Head = () => {
+  const [searchQuery, setSearchQuery] = useState("");
+  const [suggession, setSuggession] = useState([]);
+  const [showSuggestions, setShowSuggestions] = useState(false);
+  useEffect(() => {
+    const timer = setTimeout(() => getSearchSuggessions(), 200);
+    return () => {
+      clearTimeout(timer);
+    };
+  }, [searchQuery]);
+
+  const getSearchSuggessions = async () => {
+    const response = await fetch(YOUTUBE_SEARCH_API + searchQuery);
+    const json = await response.json();
+    setSuggession(json[1]);
+  };
   const dispatch = useDispatch();
   const toggleMenuHandler = () => {
     dispatch(toggleMenu());
@@ -20,14 +37,34 @@ const Head = () => {
         <img className="h-8 ml-2" src={YOUTUBE_LOGO} alt="youtube" />
       </div>
       <div className="col-span-10">
-        <input
-          className="w-1/2 border border-gray-400 p-1  rounded-l-full"
-          type="text"
-          placeholder="search..."
-        />
-        <button className="border border-gray-400 p-1 rounded-r-full">
-          🔍
-        </button>
+        <div>
+          <input
+            className="px-5 w-1/2 border border-gray-400 p-1  rounded-l-full"
+            type="text"
+            placeholder="search..."
+            value={searchQuery}
+            onChange={(e) => setSearchQuery(e.target.value)}
+            onFocus={() => setShowSuggestions(true)}
+            onBlur={() => setShowSuggestions(false)}
+          />
+          <button className="border border-gray-400 p-1 rounded-r-full">
+            🔍
+          </button>
+        </div>
+        {showSuggestions && (
+          <div className="fixed bg-white py-2 px-2 w-[45rem] shadow-lg rounded-lg border border-gray-100">
+            <ul>
+              {suggession.map((s, index) => (
+                <li
+                  key={index}
+                  className="py-2 px-3 shadow-sm hover:bg-gray-100 "
+                >
+                  🔍 {s}
+                </li>
+              ))}
+            </ul>
+          </div>
+        )}
       </div>
       <div className="col-span-1">
         <img
